908868c390809a36a62568694744b4bcf6eaa9c6
DTDwind <sam841009@yahoo.com.tw>
2019-04-30 18:55:18 +0800

    add introduction

---
+++ js/product.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("#m_introduction").click(function(){
+        alert(1)
+        // $("html,body").animate({scrollTop:$("#p1").offset().top-80}, 800);
+    }); //代表一個完整的執行區塊
+
+})
